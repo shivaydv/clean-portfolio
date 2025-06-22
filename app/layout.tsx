299e892d78e1d9ea1531fff076e0e717e5b3d075
Shiva Yadav <shivaydv1256@gmail.com>
2025-06-22 15:31:37 +0530

    opengrapgh images

--- app/layout.tsx
+++ app/layout.tsx
@@ -54,12 +54,22 @@ export const metadata: Metadata = {
       "Full Stack Engineer specializing in React, Next.js, and modern web technologies.",
     url: "https://shivayadav.me",
     siteName: "Shiva Yadav Portfolio",
+    images: [
+      {
+        url: "/images/opengrapgh-image.png",
+        width: 1200,
+        height: 630,
+        alt: "Shiva Yadav Portfolio",
+      },
+    ],
     locale: "en_US",
     type: "website",
   },
   twitter: {
     card: "summary_large_image",
     title: "Shiva Yadav | Portfolio",
+    images: ["/images/opengrapgh-image.png"],
+    site:"shivayadav.me",
     description:
       "Frontend developer specializing in React, Next.js, and modern web technologies.",
     creator: "@Shivay1256",
